b3cf1a9a266c8ce0df3bed81f9b9b7e4fd003fcd
Administrator <1367714121@qq.com>
2025-05-14 15:42:54 +0800

    5.13

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,9 +1,12 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
@@ -12,10 +15,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -75,5 +75,26 @@ public class EmployeeController {
     public Result<String> logout() {
         return Result.success();
     }
+
+    //新增员工
+    @PostMapping
+    @ApiOperation("新增员工")
+    public Result add(@RequestBody EmployeeDTO employeeDTO) {
+        log.info("新增员工：{}", employeeDTO);
+        employeeService.add(employeeDTO);
+        return Result.success();
+    }
+
+    //员工分页查询
+    @GetMapping("/page")
+    @ApiOperation("员工分页查询")
+    public Result<PageResult> page(EmployeePageQueryDTO empDTO) {
+        log.info("员工分页，参数为：{}", empDTO);
+        PageResult pageResult = employeeService.page(empDTO);
+        return Result.success(pageResult);
+    }
+
+
+
 
 }
